69e2ee073ab749c5f80cdec3db877d37c5cee48b
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-29 19:44:55 -0600

    Arreglos menores en el funcionamiento del programa, corregi algunas entradas para que sean validas y se guarden correctamente

--- actividad_10.py
+++ actividad_10.py
@@ -1,6 +1,6 @@
 prods = {}
 cat = ('Hombre', 'Mujer', 'Niño')
-tall = ('S','M''L','XL')
+tall = ('S','M','L','XL')
 error_mesagge = '-'*50+'\n'+"✖"*5+"   Lo siento, intentelo nuevamente   "+"✖"*5
 
 def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA VALIDA
@@ -11,7 +11,10 @@ def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA V
         except ValueError: print(error_mesagge)
     return value
 
-n = input_integer("▶  Ingresa la cantidad que deseas agreagar: ")
+while True:
+    n = input_integer("▶  Ingresa la cantidad que deseas agreagar: ")
+    if n > 0: break
+    else: print(error_mesagge)
 for i in range(n):
     print("-"*20+f"PRODUCTO {i+1}"+"-"*20)
     while True:
@@ -19,10 +22,16 @@ for i in range(n):
         if code in prods: print("\nCodigo existente intente de nuevo")
         else: break
     prod = input("▶  Ingresa el nombre de tu producto: ")
-    print("   1) Hombre\n   2) Mujer\n   3) Niño")
-    cat_sel =  input_integer("▶  Ingresa la categoria: ")
-    print("   1) S\n   2) M\n   3) L\n   4) XL")
-    tal = input_integer("▶  Ingresa la talla: ")
+    while True:
+        print("   1) Hombre\n   2) Mujer\n   3) Niño")
+        cat_sel =  input_integer("▶  Ingresa la categoria: ")
+        if 0 < cat_sel <=3: break
+        else: print(error_mesagge)
+    while True:
+        print("   1) S\n   2) M\n   3) L\n   4) XL")
+        tal = input_integer("▶  Ingresa la talla: ")
+        if 0 < tal <= 4: break
+        else: print(error_mesagge)
     while True:
         while True:
             try:
@@ -43,23 +52,23 @@ for i in range(n):
         'c/u': price,
         'stock' : stock
     }
-print("-"*15+"PRODUCTOS"+"-"*15)
-print(f"{'codigo':<10}{'nombre':<50}{'categoria':<20}{'talla':<20}{'Precio C/U':<20}{'Stock':<20}{'Valor Total':<25}")
+print("-"*25+"PRODUCTOS"+"-"*25)
+print(f"{'Codigo':<10}{'Nombre':<25}{'Categoria':<20}{'Talla':<20}{'Precio C/U':<20}{'Stock':<20}{'Valor Total':<25}")
 for i,j in prods.items():
-    print(f"{i:<10}{j['name']:<50}{j['categoria']:<20}{j['talla']:<20}{j['c/u']:<20}{j['stock']:<20}{j['c/u']*j['stock']:<25}")
+    print(f"{i:<10}{j['name']:<25}{j['categoria']:<20}{j['talla']:<20}{'Q'+str(j['c/u']):<20}{j['stock']:<20}{'Q'+str(j['c/u']*j['stock']):<25}")
 
-code = input("▶  Ingresa un codigo (Ej: P001): ")
-if code in prods:
-    print(f"{'nombre':<50}{'categoria':<20}{'talla':<20}{'Precio C/U':<20}{'Stock':<20}{'Valor Total':<25}")
-
-    print(f"{prods[code]['name']:<50}{prods[code]['categoria']:<20}{prods[code]['talla']:<20}{prods[code]['c/u']:<20}{prods[code]['stock']:<20}{prods[code]['c/u']*prods[code]['stock']:<25}")
-else: print("Lo sentimos, el codigo no existe")
-
-print("-"*15+"PRODUCTOS POR CATEGORIA"+"-"*15)
-print(f"{'categoria':<20} {'cantidad':<10}")
+print("-"*25+"PRODUCTOS POR CATEGORIA"+"-"*25)
+print(f"{'Categoria':<20} {'Cantidad':<10}")
 for i in cat:
     count = 0
     for j in prods.values():
         if j['categoria'] == i: count+=1
 
     print(f"{i:<20}{count:<10}")
+
+code = input("\n\n▶  Ingresa un codigo para visualizar su información por separado: ")
+if code in prods:
+    print(f"{'Nombre':<25}{'Categoria':<20}{'Talla':<20}{'Precio C/U':<20}{'Stock':<20}{'Valor Total':<25}")
+    print(f"{prods[code]['name']:<25}{prods[code]['categoria']:<20}{prods[code]['talla']:<20}{'Q'+str(prods[code]['c/u']):<20}{prods[code]['stock']:<20}{'Q'+str(prods[code]['c/u']*prods[code]['stock']):<25}")
+else: print("Lo sentimos, el codigo no existe")
+
